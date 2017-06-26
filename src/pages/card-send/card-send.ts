c05efce9217bcad712a788811c10ea377f9f1bf0
Travis Jones <Travis Jones>
2017-06-26 16:35:32 -0400

    Working on observable toast.

--- src/pages/card-send/card-send.ts
+++ src/pages/card-send/card-send.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { McCardService } from "../../app/services/card-service";
 import { McCard } from "../../app/interfaces/card";
+import { EmailComposer } from "@ionic-native/email-composer";
+import {ActionSheetController, Platform} from "ionic-angular";
 
 @Component({
     selector: 'page-card-send',
     templateUrl: 'card-send.html',
-    providers: [McCardService]
+    providers: [McCardService, EmailComposer]
 })
 /**
  * This is the class for the component that handles the view for sending cards.
  */
 export class McCardSend {
 
+    private canEmail:boolean = false;
     private card:McCard = {
         image_url: null,
         full_name: null,
@@ -28,9 +31,18 @@ export class McCardSend {
      * Constructor
      *
      * @param cardService
+     * @param emailComposer
+     * @param platform
+     * @param actionSheetCtrl
      * @return void
      */
-    constructor(private cardService: McCardService) {}
+    constructor(private cardService: McCardService, private emailComposer: EmailComposer, private platform: Platform, public actionSheetCtrl: ActionSheetController) {
+
+        let self:McCardSend = this;
+
+        self.checkEmailAvailability()
+
+    }
 
     /**
      * Retrieves the new card each time it enters the view.
@@ -48,6 +60,38 @@ export class McCardSend {
 
                 }
             );
+
+    }
+
+    /**
+     * Checks to see if the email composer is available.
+     */
+    private checkEmailAvailability():void {
+
+        let self:McCardSend = this;
+
+        if(self.platform.is('cordova')) {
+
+            self.emailComposer.isAvailable().then((available: boolean) =>{
+
+                if(available) {
+
+                    //Now we know we can send emails
+                    self.canEmail = true;
+
+                }
+
+            });
+
+        }
+
+    }
+
+    private send():void {
+
+        let self:McCardSend = this;
+
+
 
     }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,78 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Platform, Toast, ToastController} from 'ionic-angular';
 import { McCardEdit } from '../card-edit/card-edit';
 import { McCardSend } from '../card-send/card-send';
+import { EmailComposer } from "@ionic-native/email-composer";
+import {ArrayObservable} from "rxjs/observable/ArrayObservable";
+import {Observable} from "rxjs/Observable";
 
 @Component({
     selector: 'page-home',
-    templateUrl: 'home.html'
+    templateUrl: 'home.html',
+    providers: [EmailComposer, ToastController]
 })
 export class McHomePage {
 
     private cardSend:any;
     private cardEdit:any;
+    private notices:Observable<Array<any>>;
 
-    constructor(public navCtrl: NavController) {
+    /**
+     * Constructor
+     *
+     * @param emailComposer
+     * @param toastCtrl
+     * @param platform
+     */
+    constructor(private emailComposer: EmailComposer, private toastCtrl: ToastController, private platform: Platform) {
 
         let self:McHomePage = this;
 
         self.cardEdit = McCardEdit;
         self.cardSend = McCardSend;
+
+        self.notices.subscribe((fn:any) => {
+
+        });
+        self.alertIfEmailUnavailable();
+
+    }
+
+    /**
+     * Pops up toast if the device isn't able to be sent.
+     */
+    public alertIfEmailUnavailable():void {
+
+        let self:McHomePage = this;
+
+        // Put the toast in the toaster.
+        let toast = self.toastCtrl.create({
+            message: 'Unable to send your card via email.',
+            duration: 3000,
+            position: 'bottom'
+        });
+
+        if(self.platform.is('cordova')) {
+
+            self.emailComposer.isAvailable().then((available: boolean) =>{
+
+                if(!available) {
+
+                    self.notices.
+                    // Pop the toast.
+                    toast.present();
+
+                }
+
+            });
+
+        }
+        else {
+
+            // Pop the toast.
+            toast.present();
+
+        }
 
     }
 
